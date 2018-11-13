4d67e4ff04b0ee37965e2edf9753287b7cd8d91b
jtr <jtr354@foxmail.com>
2018-11-13 17:05:19 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import 'utils/app-plugins'
+
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
