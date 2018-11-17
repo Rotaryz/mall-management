40d9a760b3524b156bed808efb36861781e8d069
jtr <jtr354@foxmail.com>
2018-11-17 10:42:31 +0800

    提交代码

--- src/components/base-panel/base-panel.vue
+++ src/components/base-panel/base-panel.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="base-panel" :style="styles">
-    <header></header>
     <slot></slot>
     <footer>
-
+      <page-detail :pageDtail="pageDetails" @addPage="addPage"></page-detail>
     </footer>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import pageDetail from 'components/page-detail/page-detail'
+
   export default {
+    components: {
+      pageDetail,
+    },
     props: {
       styles: {
         type: String,
-        default: ''
-      }
-    }
+        default: '',
+      },
+      pageDetails: {
+        type: Object,
+        default: () => {
+          return {
+            total: 1, // 总数量
+            per_page: 1, // 一页条数
+            total_page: 1, // 总页数
+          }
+        },
+      },
+    },
+    methods: {
+      addPage(page) {
+        this.$emit('navToPage', page)
+      },
+    },
   }
 </script>
 
@@ -27,7 +46,14 @@
     margin: 22px
     background: #FFFFFF
     box-shadow: 0 1px 6px 0 rgba(0, 8, 39, 0.10)
-    overflow :hidden
+    overflow: hidden
     border-radius: 6px
     flex: 1
+    & > footer
+      height: 121px
+      layout(row)
+      justify-content :center
+      align-items :center
+      padding : 0 30px 0 39px
+
 </style>
