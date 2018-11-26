c917d9fa682f881c53f68b6943cba570c1860ceb
jtr <jtr354@foxmail.com>
2018-11-26 15:22:31 +0800

    添加预览

--- src/pages/goods-detail/goods-review/goods-review.vue
+++ src/pages/goods-detail/goods-review/goods-review.vue
@@ -1,13 +1,13 @@
 <template>
   <base-modal v-show="isShow">
-    <div slot="content" class="review-wrapper">
+    <div slot="content" class="review-wrapper" :class="showActive ? 'model-active' : 'model-un-active'">
       <header>预览效果</header>
       <section class="wrapper">
-        <figure class="goods-images" v-if="goodsImage" :style="{backgroundImage: 'url(' + goodsImage.imageURL +')'} ">
-          <div>2<span>/</span>5</div>
+        <figure class="goods-images" v-if="goodsImage[0]" :style="{backgroundImage: 'url(' + goodsImage[0].imageURL +')'} ">
+          <div>1<span>/</span>{{goodsImage.length}}</div>
         </figure>
         <article class="content">
-          <div class="title">{{goodsInfo.title}}MIJIA/米家 米家IH电饭煲 4L 3-4人小型家用智能全自动小米电饭锅</div>
+          <div class="title">{{goodsInfo.title}}</div>
           <div class="number">
             <div>剩余{{goodsInfo.store}}件</div>
             <div>{{goodsInfo.isMoneyPage ? '销量':'已兑'}}{{goodsInfo.saleCount}}件</div>
@@ -51,20 +51,37 @@
     },
     data() {
       return {
-        isShow: false
+        isShow: false,
+        showActive: false,
+        timer: null
       }
+    },
+    destroyed() {
+      this.timer && clearTimeout(this.timer)
     },
     methods: {
       show() {
+        if (this.timer) return
         this.isShow = true
+        this.showActive = true
+        this.timer = setTimeout(() => {
+          clearTimeout(this.timer)
+          this.timer = null
+        }, 500)
       },
       hide() {
+        if (this.timer) return
         this.isShow = false
+        this.showActive = false
+        this.timer = setTimeout(() => {
+          clearTimeout(this.timer)
+          this.timer = null
+        }, 500)
       }
     },
     computed: {
       goodsImage() {
-        return this.goodsInfo.goodsImages[0]
+        return this.goodsInfo.goodsImages
       },
       detailImages() {
         return this.goodsInfo.detailImages
@@ -80,43 +97,73 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  $wrapper-width = 356px
+  $wrapper-width = 375px
+  $wrapper-height = (60.5+667+50)px
   @import "~common/stylus/variable"
   @import '~common/stylus/mixin'
 
   .h-10
     height :10px
-    background: #F5F5F9;
+    background: #F5F5F9
+
+  .model-active
+    animation: layerFadeIn .3s
+
+  .model-un-active
+    animation: hideFadeIn .4s
+
+  @keyframes layerFadeIn {
+    0% {
+      opacity: 0
+      transform: scale(.5)
+    }
+    100% {
+      opacity: 1
+      transform: scale(1)
+    }
+  }
+
+  @keyframes hideFadeIn {
+    0% {
+      opacity: 1
+      transform: scale(1)
+    }
+    100% {
+      transform: scale(.5)
+      opacity: 0
+    }
+  }
 
   .review-wrapper
-    height :900px
+    height :$wrapper-height
     width :$wrapper-width
     background :$color-white
-    box-shadow: 0 0 5px 0 rgba(12,6,14,0.60);
-    border-radius: 3px;
+    box-shadow: 0 0 5px 0 rgba(12,6,14,0.60)
+    border-radius: 3px
     layout()
     header
       height :60.5px
-      font-family: PingFangSC-Medium;
-      font-size: 16px;
-      color: #000000;
-      text-align: center;
+      font-family: PingFangSC-Medium
+      font-size: 16px
+      color: #000000
+      text-align: center
       line-height: @height
+      user-select :none
     footer
       cursor :pointer
       height :50px
-      font-family: PingFangSC-Regular;
-      font-size: 16px;
-      color: #FFFFFF;
-      text-align: center;
+      font-family: PingFangSC-Regular
+      font-size: 16px
+      color: #FFFFFF
+      text-align: center
       line-height: @height
       background :$color-main
+      user-select :none
     .wrapper
       flex: 1
-      overflow :auto
-      position :relative
+      overflow-x :hidden
+      overflow-y:auto
       &::-webkit-scrollbar
-        position :absolute
         width: 8px
         height: 10px
       &::-webkit-scrollbar-thumb
@@ -139,13 +186,13 @@
           position :absolute
           right :14.8px
           bottom :10.5px
-          background: rgba(0,0,0,0.3);
-          border-radius: 9.49px;
+          background: rgba(0,0,0,0.3)
+          border-radius: 9.49px
           text-align :center
           line-height :@height
-          font-family: DINAlternate-Bold;
-          font-size: 12px;
-          color: #FFFFFF;
+          font-family: DINAlternate-Bold
+          font-size: 12px
+          color: #FFFFFF
           padding :0 6px
           vertical-align :bottom
           &:first-letter
@@ -157,19 +204,19 @@
       .content
         padding :13px 11px 18.5px
         .title
-          font-family: PingFangSC-Medium;
-          font-size: 14px;
-          color: #1F1F1F;
+          font-family: PingFangSC-Medium
+          font-size: 14px
+          color: #1F1F1F
           line-height: 1.2
           no-wrap-plus()
           text-align :justify
           word-break :break-all
         .number
           layout(row,block,nowrap)
-          font-family: PingFangSC-Regular;
-          font-size: 12px;
-          color: #6E6E6E;
-          line-height: 12px;
+          font-family: PingFangSC-Regular
+          font-size: 12px
+          color: #6E6E6E
+          line-height: 12px
           margin-top :8.8px
           div:last-child
             margin-left :17.5px
@@ -182,32 +229,32 @@
             color: $color-main
             align-items :center
             .unit
-              font-family: PingFangSC-Medium;
-              font-size: 12px;
+              font-family: PingFangSC-Medium
+              font-size: 12px
               margin-right 1.1px
             .money
-              font-family: DINAlternate-Bold;
-              font-size: 18px;
+              font-family: DINAlternate-Bold
+              font-size: 18px
             .credits
               margin-right 1.1px
-              font-family: DINAlternate-Bold;
-              font-size: 18px;
+              font-family: DINAlternate-Bold
+              font-size: 18px
             .c-text
-              font-family: PingFangSC-Regular;
-              font-size: 16px;
+              font-family: PingFangSC-Regular
+              font-size: 16px
           .origin
             margin-left :7px
-            font-family: PingFangSC-Regular;
-            font-size: 14px;
-            color: #B1B1B1;
+            font-family: PingFangSC-Regular
+            font-size: 14px
+            color: #B1B1B1
             text-decoration :line-through
       .detail-wrapper
         .title
           padding :14px 11px 12px
-          font-family: PingFangSC-Medium;
-          font-size: 14px;
-          color: #1F1F1F;
-          line-height: 14px;
+          font-family: PingFangSC-Medium
+          font-size: 14px
+          color: #1F1F1F
+          line-height: 14px
         dd
           box-sizing :border-box
 </style>
